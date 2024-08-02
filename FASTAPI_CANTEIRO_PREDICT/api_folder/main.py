3b83bbbf7d93bab95bb4de3700782263613087fb
Mateus <mateusreis.ufmt@gmail.com>
2024-08-02 14:34:43 -0400

    fix: brigde/canteiro get change to post

--- FASTAPI_CANTEIRO_PREDICT/api_folder/main.py
+++ FASTAPI_CANTEIRO_PREDICT/api_folder/main.py
@@ -6,7 +6,7 @@ from fastapi_versioning import VersionedFastAPI, version
 
 app = FastAPI()
 
-@app.get("/median-exists/")
+@app.post("/median-exists/")
 @version(1)
 async def predict(
         lat: float = Form(None),
