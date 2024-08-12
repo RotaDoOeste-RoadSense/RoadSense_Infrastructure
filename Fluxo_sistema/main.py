9d3b773e608a6ec72a7eae616f4dcf081c7a5e57
jcss4silva <jcss4@cin.ufpe.br>
2024-08-12 13:15:52 -0400

    updated main.py

--- Fluxo_sistema/main.py
+++ Fluxo_sistema/main.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # import get_desired_folder
 # folder = get_desired_folder.pasta
-folder = "/mnt/teste2/Viagem3/"
+folder = "/mnt/internal/Viagem4/"
 import receber_nova_trip
 trip_id = receber_nova_trip.main(folder)
 # print(folder,trip_id)
@@ -16,7 +16,7 @@ create_gps_table(os.path.join(folder,'Panoramic'),trip_id)
 # encontrar_todas_placas(os.path.join(folder,'images'),trip_id)
 # from encontrar_gps_todas_placas import run as encontrar_gps_todas_placas
 # encontrar_gps_todas_placas(os.path.join(folder,'images'),trip_id)
-
-
+from encontrar_todas_defensas import run as encontrar_todas_defensas
+encontrar_todas_defensas(os.path.join(folder,'Cube'),trip_id)
 
 
