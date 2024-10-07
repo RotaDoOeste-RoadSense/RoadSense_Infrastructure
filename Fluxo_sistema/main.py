98853f875d9cd56a8efe56b4c810d5cd718724ad
jcss4silva <jcss4@cin.ufpe.br>
2024-10-07 11:05:41 -0400

    new updates for guardrail flow

--- Fluxo_sistema/main.py
+++ Fluxo_sistema/main.py
@@ -5,12 +5,11 @@ import pandas as pd
 # Folder sem a ultima barra
 
 folder = "/mnt/teste/GPS_norte_from43"
-trip_id = 23
+trip_id = 1
 trip_direction = 'NORTE' # ou 'SUL'
 
-
-#tabela trips
 '''
+#tabela trips
 import receber_nova_trip
 trip_id = receber_nova_trip.main(folder)
 
@@ -54,8 +53,7 @@ table_gps(trip_id, 'GPS_norte.xlsx')
 
 #load CRO guardrails
 from load_cro import *
-#load_guardrails('defensas_total_2024.xlsx') 
-#create_geometries() 
+load_guardrails('defensas_total_2024.xlsx') 
 
 # predict guardrails
 from encontrar_todas_defensas import run as encontrar_todas_defensas
