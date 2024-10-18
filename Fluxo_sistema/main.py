2296e70ac9a512ce0cf9a32c755d7abe9f0aff0f
jcss4silva <jcss4@cin.ufpe.br>
2024-10-18 18:32:42 -0400

    some code updates

--- Fluxo_sistema/main.py
+++ Fluxo_sistema/main.py
@@ -3,18 +3,21 @@ import pandas as pd
 
 # Folder sem a ultima barra
 
-folder = "/mnt/windows_share/{nome_pasta}"
-#trip_id = 1
+folder = "/mnt/teste/GPS_norte_from43"
+trip_id = 1
 trip_direction = 'NORTE' # ou 'SUL'
 
+'''
 #tabela trips
 import receber_nova_trip
 trip_id = receber_nova_trip.main(folder)
 
 # # tabela GPS
 from utils import run as table_gps
-table_gps(trip_id, 'nome_pasta.xlsx')
+table_gps(trip_id, 'GPS_norte.xlsx')
+'''
 
+'''
 from encontrar_todas_placas import run as encontrar_todas_placas
 encontrar_todas_placas(os.path.join(folder,'Cube'),trip_id)
 
@@ -27,22 +30,25 @@ encontrar_gps_todas_placas(os.path.join(folder,'images'),trip_id)
 from analyze_plate_quality import main as analyze_plate_quality_main
 analyze_plate_quality_main(trip_id)
 
-
 from criar_trechos import run as criar_trechos
 criar_trechos(trip_id)
 
 from classifica_vegetacao import run as classificar_vegetacao
 classificar_vegetacao(trip_id)
+'''
 
 #load some CRO elements
-#from load_cro import *
-#load_guardrails('defensas_total_2024.xlsx') 
-#load_drainages('DS_clean')
+from load_cro import *
+load_guardrails('defensas_total_2024.xlsx') 
+load_drainages('DS_clean')
 
 
 # predict guardrails
 #from encontrar_todas_defensas import run as encontrar_todas_defensas
 #encontrar_todas_defensas(os.path.join(folder,'Cube'),trip_id,trip_direction)
+from tools_cont_els import *
+adjust_pos(os.path.join(folder,'Cube'), trip_id)
+
 '''
 # usar abaixo para estimar defensas unicas sem dados externos....
 #from pred_defensas import run as pred_defensas
@@ -53,4 +59,3 @@ classificar_vegetacao(trip_id)
 
 #from encontrar_todas_drenagens import run as encontrar_todas_drenagens
 #encontrar_todas_drenagens(os.path.join(folder,'Cube'),trip_id,trip_direction)
-
--- Fluxo_sistema/tools_cont_els.py
+++ Fluxo_sistema/tools_cont_els.py
@@ -1,9 +1,84 @@
-import cv2
 import torch
-import urllib.request
+import timm
+from PIL import Image
+from PIL.ExifTags import TAGS
+import cv2
+import numpy as np
+print("timm ver:" + str(timm.__version__))
+import requests
+import os
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, Column, Integer, String, Float, BigInteger,asc, Table, MetaData, select, func
+from sqlalchemy.orm import sessionmaker
+import yaml
 
-def get_depth(img_):
+# load zoedepth model from torch hub...
+torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)  # Triggers fresh download of MiDaS repo
+repo = "isl-org/ZoeDepth"
+# Zoe_N trained in NYU Depth v2 (N) for indoor scenes, but worked better than other models in the highway images....
+model_zoe_n = torch.hub.load(repo, "ZoeD_N", pretrained=True)
+
+#database config
+Base = declarative_base()
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+database_url = cfg['database']['url']
+engine = create_engine(database_url)
+
+def predict_depth(folder, image_name):
+    # Load and preprocess the input image
+    input_image_path = os.path.join(folder,image_name)  # Replace with your image path
+    image = cv2.imread(input_image_path)  # Read the image using OpenCV
+    predicted_depth = model_zoe_n.infer_pil(image, pad_input=False) 
+    return predicted_depth
+
+def getbbox_centroid_depth(detection, predicted_depth):
+    bbox = detection['xyxy']
+    # Calculate the centroid of the bounding box
+    x_min, y_min, x_max, y_max = bbox
+    centroid_x = int((x_min + x_max) / 2)
+    centroid_y = int((y_min + y_max) / 2)
+    # get centroid depth
+    centroid_depth = predicted_depth[centroid_x,centroid_y]
+    return centroid_depth
+
+def adjust_pos(folder, trip_id):
+    # Create a metadata instance
+    metadata = MetaData()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    pred_guardrail_points = Table('pred_guardrails_with_geom', metadata, autoload_with=engine)
+    guardrail_details = Table('guardrail_details', metadata, autoload_with=engine)
+    image_points = Table('image_data_with_geom', metadata, autoload_with=engine)
+
+    session = Session() 
+
+    # Query to group guardrail_details by unique_id and get associated image names
+    query = (
+        select(guardrail_details.c.unique_id, guardrail_details.c.pred_true, image_points.c.image_name)
+        .select_from(guardrail_details.join(image_points, guardrail_details.c.image_id == image_points.c.image_id))
+        .group_by(guardrail_details.c.unique_id, image_points.c.image_name)
+    )
+
+    results = session.execute(query).fetchall()
+
+    # Process and print the results
+    guardrail_images = {}
+    for unique_id, image_name in results:
+        if unique_id not in guardrail_images:
+            guardrail_images[unique_id] = []
+        guardrail_images[unique_id].append(image_name)
+
+    for unique_id, images in guardrail_images.items():
+        print(f"Guardrail {unique_id} has images: {', '.join(images)}")
+        predicted_depth = predict_depth(folder,image_name)
+        centr_depth = getbbox_centroid_depth(detection, predicted_depth)
+
+    session.close()
 
 
-def get_loc_element(img_set, lado):
-    for img in img_set:
+
+
+
