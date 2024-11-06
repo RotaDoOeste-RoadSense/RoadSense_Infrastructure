75560f93d402adf1c02eff9cc7ec250d9b8faa8d
jcss4silva <jcss4@cin.ufpe.br>
2024-11-06 09:18:48 -0400

    updated main drainage, guardrails flows

--- Fluxo_sistema/main_drenagem.py
+++ Fluxo_sistema/main_drenagem.py
@@ -17,45 +17,9 @@ from utils import run as table_gps
 table_gps(trip_id, 'GPS_norte.xlsx')
 '''
 
-'''
-from encontrar_todas_placas import run as encontrar_todas_placas
-encontrar_todas_placas(os.path.join(folder,'Cube'),trip_id)
-
-from get_blue_plates import main as blue_plates
-blue_plates(trip_id, folder)
-
-from encontrar_gps_todas_placas import run as encontrar_gps_todas_placas
-encontrar_gps_todas_placas(os.path.join(folder,'images'),trip_id)
-
-from analyze_plate_quality import main as analyze_plate_quality_main
-analyze_plate_quality_main(trip_id)
-
-from criar_trechos import run as criar_trechos
-criar_trechos(trip_id)
-
-from classifica_vegetacao import run as classificar_vegetacao
-classificar_vegetacao(trip_id)
-'''
-
 #load some CRO elements
 from load_cro import *
-load_guardrails('defensas_total_2024.xlsx') 
 load_drainages('DS_clean')
 
-
-# predict guardrails
-from encontrar_todas_defensas import run as encontrar_todas_defensas
-encontrar_todas_defensas(os.path.join(folder,'Cube'),trip_id,trip_direction)
-#from tools_cont_els import *
-#adjust_pos(os.path.join(folder,'Cube'), trip_id)
-
-'''
-# usar abaixo para estimar defensas unicas sem dados externos....
-#from pred_defensas import run as pred_defensas
-#pred_defensas(os.path.join(folder,'Cube'),trip_id,trip_direction)
-#from pred_defensas import find_unique_defensas as find_unique
-#find_unique(trip_id,trip_direction)
-'''
-
-#from encontrar_todas_drenagens import run as encontrar_todas_drenagens
-#encontrar_todas_drenagens(os.path.join(folder,'Cube'),trip_id,trip_direction)
+from encontrar_todas_drenagens import run as encontrar_todas_drenagens
+encontrar_todas_drenagens(os.path.join(folder,'Cube'),trip_id,trip_direction)
