274353bf41f86c53314a3488338051db996b10d5
Victor Medeiros <victormedeiros@rotadooeste.com.br>
2024-11-27 12:23:30 -0400

    feat: created the table regarding horizontal markings.

---
+++ APIs/SQL/scripts/victor.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS "horizontal_markings";
+CREATE TABLE "horizontal_markings" (
+  "horizontal_markings_id" SERIAL PRIMARY KEY,
+  "class_id" SMALLINT,
+  "class_name" CHAR(20),
+  "mask_polygon" POLYGON,
+  "quality_score" REAL,
+  "image_id" INT REFERENCES "image_data"("image_id")
+);
