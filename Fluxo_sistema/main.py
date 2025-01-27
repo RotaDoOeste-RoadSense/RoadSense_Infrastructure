68d8add1254948de17e50efe78ef654f7be87225
Victor Medeiros <victormedeiros96@gmail.com>
2025-01-27 10:14:17 -0400

    Add RabbitMQ integration for task messaging and update requirements

--- Fluxo_sistema/main.py
+++ Fluxo_sistema/main.py
@@ -1,3 +1,30 @@
+import pika
+import json
+import time
+RABBITMQ_HOST = '192.168.18.254'
+def connect_to_rabbit():
+    while True:
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=RABBITMQ_HOST,
+                                          credentials=pika.PlainCredentials(username='rdt', password='123456')
+                                          )
+            )
+            return connection
+        except pika.exceptions.AMQPConnectionError:
+            print("Tentando se conectar ao RabbitMQ...")
+            time.sleep(5)
+def send_task(queue_name, task_message):
+    channel.queue_declare(queue=queue_name, durable=True)
+    channel.basic_publish(
+        exchange='',
+        routing_key=queue_name,
+        body=json.dumps(task_message),
+        properties=pika.BasicProperties(delivery_mode=2)
+    )
+    print(f"[x] Mensagem enviada para {queue_name}: {task_message}")
+
+
 # Folder sem a ultima barra
 
 folder = "/mnt/windows_share/GPS"
@@ -12,49 +39,8 @@ trip_id = receber_nova_trip.main(folder, trip_direction)
 from utils import run as table_gps
 table_gps(trip_id, 'GPS_norte_amostra.xlsx')
 
-'''
-from Fluxo_sistema.Placas.encontrar_todas_placas import run as encontrar_todas_placas
-#encontrar_todas_placas(os.path.join(folder,'Cube'),trip_id)
-
-from get_blue_plates import main as blue_plates
-#blue_plates(trip_id, folder)
-
-from encontrar_gps_todas_placas import run as encontrar_gps_todas_placas
-#encontrar_gps_todas_placas(os.path.join(folder,'Cube'),trip_id)
-
-from analyze_plate_quality import main as analyze_plate_quality_main
-#analyze_plate_quality_main(trip_id)
-
-from criar_trechos import run as criar_trechos
-#criar_trechos(trip_id)
-
-from classifica_vegetacao import run as classificar_vegetacao
-classificar_vegetacao(trip_id)
-
-#classificar_vegetacao(1)
-#classificar_vegetacao(2)
-
-#load some CRO elements
-from load_cro import *
-load_guardrails('defensas_total_2024.xlsx') 
-load_drainages('DS_clean')
-
-
-# predict guardrails
-from encontrar_todas_defensas import run as encontrar_todas_defensas
-encontrar_todas_defensas(os.path.join(folder,'Cube'),trip_id,trip_direction)
-from tools_cont_els import *
-adjust_pos(os.path.join(folder,'Cube'), trip_id)
-'''
-
-
-'''
-# usar abaixo para estimar defensas unicas sem dados externos....
-#from pred_defensas import run as pred_defensas
-#pred_defensas(os.path.join(folder,'Cube'),trip_id,trip_direction)
-#from pred_defensas import find_unique_defensas as find_unique
-#find_unique(trip_id,trip_direction)
-'''
-
-#from encontrar_todas_drenagens import run as encontrar_todas_drenagens
-#encontrar_todas_drenagens(os.path.join(folder,'Cube'),trip_id,trip_direction)
+for queue in ['Placa','Matinho']:
+    connection = connect_to_rabbit()
+    channel = connection.channel()
+    send_task(queue, {"trip_id": trip_id, "trip_direction": trip_direction, "folder": folder})
+    connection.close()
