e934e2fd087fc8f42cdc8b103d166cb354606d38
Mateus Reis <mateusreis.ufmt@gmail.com>
2025-08-27 11:22:03 -0400

    style: change prod variable to production

--- APIs/FASTAPI_GET_NEW_TRIP/api_folder/main.py
+++ APIs/FASTAPI_GET_NEW_TRIP/api_folder/main.py
@@ -12,14 +12,14 @@ async def new_trip(
     way: Optional[str] = Form(None),
     starting_city: Optional[str] = Form(None),
     ending_city: Optional[str] = Form(None),
-    prod: bool = Form(False),
+    production: bool = Form(False),
 ):
     response_content = create_new_trip(
         root_folder=path,
         way=way,
         starting_city=starting_city,
         ending_city=ending_city,
-        prod = prod
+        production = production
     )
     return JSONResponse(content = response_content
         
